6a78188536a849c480e398a9c11d4109b90695a6
Victor Alejandro Ordoñez <vordonez74@gmail.com>
2021-07-07 10:37:52 -0300

    Agregado algunos html y links

--- appFinal/urls.py
+++ appFinal/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('/buscar', views.buscar, name="buscar"),
+    path('/acercade', views.acercade, name="acercade"),
 ]
--- appFinal/views.py
+++ appFinal/views.py
@@ -3,5 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse("Hola mundo!")
+    return render(request, "index.html")
+
+
+def buscar(request):
+    return render(request,"buscar.html")
+
+
+def acercade(request):
+    return render(request, "acercade.html")
